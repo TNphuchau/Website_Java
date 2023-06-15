aaf359d4df1760aa4935b9cf9fc9f1d25f5414b7
Myhgan <hang08hb@gmail.com>
2023-06-15 11:39:39 +0700

    .

--- sarah/src/main/java/com/web/sarah/repository/ProductRepository.java
+++ sarah/src/main/java/com/web/sarah/repository/ProductRepository.java
@@ -22,10 +22,10 @@ public interface ProductRepository extends JpaRepository<Product,Long>{
 	
 	Product findById(int id);
 	
-	@Query(value="select * from `fashionstore`.product where `fashionstore`.product.product_name like %?1% and `fashionstore`.product.category_id= ?2",nativeQuery = true)
+	@Query(value="select * from `sarah`.product where `sarah`.product.product_name like %?1% and `sarah`.product.category_id= ?2",nativeQuery = true)
 	Page<Product> findByProduct_NameAndCategory_idContaining(String name, int category_id, Pageable pageable);
 	
-	@Query(value="select * from `fashionstore`.product where `fashionstore`.product.product_name like %?1%",nativeQuery = true)
+	@Query(value="select * from `sarah`.product where `sarah`.product.product_name like %?1%",nativeQuery = true)
 	Page<Product> findByProduct_NameContaining(String name, Pageable pageable);
 	
 //	@Query(value="select * from product p where p.category_id = ?1 ORDER BY p.sold DESC LIMIT 4;",nativeQuery = true)
